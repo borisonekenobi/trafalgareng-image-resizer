f33c99075550db5035a816df8219f9812e01b7b9
Boris <borisonekenobi@gmail.com>
2022-06-27 19:14:51 -0400

    User installs installer instead of zip file

--- imageResizer/Properties/AssemblyInfo.cs
+++ imageResizer/Properties/AssemblyInfo.cs
@@ -27,10 +27,9 @@ using System.Runtime.InteropServices;
 //      Major Version
 //      Minor Version
 //      Build Number
-//      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.1.2")]
-[assembly: AssemblyFileVersion("5.1.2")]
+[assembly: AssemblyVersion("6.0.0")]
+[assembly: AssemblyFileVersion("6.0.0")]
